901c9bf8606ecf96b46a6351010fe65baae476ac
unknown <mateusandresilva2@gmail.com>
2021-03-07 13:33:28 -0300

    first link and another page

--- pages/index.js
+++ pages/index.js
@@ -1,4 +1,14 @@
+import Link from 'next/link'
+
 function Home(){
-    return <h1>Home</h1>
+    return (
+        <div>
+            <h2>Home</h2>
+
+            <Link href="/sobre">
+                <a>Acessar a pagina sobre</a>
+            </Link>
+        </div>
+        )
 } 
 export default Home
---
+++ pages/sobre.js
@@ -0,0 +1,14 @@
+import Link from 'next/link'
+
+function Sobre(){
+    return (
+        <div>
+            <h2>Sobre</h2>
+
+            <Link href="/">
+                <a>Acessar a pagina Home</a>
+            </Link>
+        </div>
+        )
+} 
+export default Sobre
